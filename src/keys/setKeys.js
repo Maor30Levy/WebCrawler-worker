85438243c3d3cd5e2651048f4f721c8446b11035
unknown <maor30levy@gmail.com>
2021-03-14 19:52:12 +0200

    3.0

--- src/keys/setKeys.js
+++ src/keys/setKeys.js
@@ -1,17 +1,16 @@
-const {saveSecret} = require('../aws/ssm');
+const { saveSecret } = require('../aws/ssm');
 const keys = require('./keys');
 
-const setKeys = async ()=>{
-    try{
+const setKeys = async () => {
+    try {
         const devPort = process.env.PORT;
         const parserHost = process.env.PARSER_HOST;
         const workerHost = process.env.WORKER_HOST;
-        await saveSecret('workerPort',devPort);
-        await saveSecret('parserHost',parserHost);
-        await saveSecret('workerHost',workerHost);
-    }catch(err){
+        await saveSecret('workerPort', devPort);
+        await saveSecret('workerHost', workerHost);
+    } catch (err) {
         console.log(err);
     }
 };
 
-module.exports = {setKeys};
+module.exports = { setKeys };
